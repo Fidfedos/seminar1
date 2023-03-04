007d4ea92a22fb8a44cf1c40ea19b52f1da1c819
Kirill <kirill_ve@bk.ru>
2023-03-04 15:25:26 +0300

    tusk20

---
+++ tusk20/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+
+//Возведение в степень числа.
+// int a = 5;
+// int b = 3;
+// double res = Math.pow(a,b);
+// Console.WriteLine("res");
+//AB = √(xb - xa)2 + (yb - ya)2
+
+int getInt()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+int x1 = getInt();
+int y1 = getInt();
+int x2 = getInt();
+int y2 = getInt();
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+Console.WriteLine(d);
