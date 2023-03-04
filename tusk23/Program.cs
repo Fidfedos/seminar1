c5596baa7cc4ea6b57980a06db080d0a816bcf82
Kirill <kirill_ve@bk.ru>
2023-03-04 16:41:26 +0300

    tusk23

---
+++ tusk23/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+void B(int[] B)
+{
+  int counter = 0;
+  int length = B.Length;
+  while (counter <  length)
+  {
+    B[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  while(index < count)
+  {
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[A+1];
+B(arry);
+PrintArry(arry);
