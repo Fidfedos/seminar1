1e996e1e0c0d52e56ae517cb0e05993e802fd875
Kirill <kirill_ve@bk.ru>
2023-03-26 13:05:26 +0300

    tusk69

---
+++ tusk69/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int a = GetNumberFromUser($"Введите число ", "Ошибка ввода!");  
+int b = GetNumberFromUser($"Введите число ", "Ошибка ввода!");  
+int result = GetResult(a, b);
+Console.Write($"A = {a}; B = {b} -> {result}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetResult(int a, int b)
+{
+    if (b == 1) return a;
+    {
+        return a*GetResult(a, b-1);
+    }
+}
